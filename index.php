70fc39e2127a3ff29b8ab66091bed8b010de1929
laxmicoin <sohan@laxmicoin.com>
2018-03-27 12:55:12 +0530

    website

---
+++ index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="laxmicoin, lakshmicoin">
+<meta name="description" content="Empower consumers around the world">
+<meta name="google-site-verification" content="BHlwV6mX-ZA0b7k1ePIlV4YfTtuZZ16l_xhuc_h8osc" />
+<title>Laxmicoin&reg; Platform : Empower consumers around the world.</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<!--FontAwesome Font Style -->
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<!-- Fav and touch icons -->
+<link rel="shortcut icon" href="images/favicon.png">
+<!-- Google-Font-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109038494-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-109038494-1');
+</script>
+
+</head>
+<body id="home">
+<!-- Header -->
+<header id="header" class="nav-stacked affix-top" data-spy="affix" data-offset-top="10">
+  <!-- Navigation -->
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+      	<div class="logo"> <a href="https://laxmicoin.com/"><img src="images/logo.png" alt="image"/></a> </div>
+        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> 
+            <span class="sr-only">Toggle navigation</span> 
+            <span class="icon-bar"></span> 
+            <span class="icon-bar"></span> 
+            <span class="icon-bar"></span> 
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="#intro" class="js-target-scroll">Home</a></li>
+          <li><a href="#about_us" class="js-target-scroll">About Us</a></li>
+          <li><a href="#road_map" class="js-target-scroll">Road Map</a></li>
+          <li><a href="#socialserv" class="js-target-scroll">Rover</a></li>
+          <li><a href="https://laxmicoin.com/faq.html" target="_blank">FAQ</a></li>
+          <li><a href="#newsletter" style="border:1px solid;margin:11px;padding:11px;" class="js-target-scroll">SignUp</a></li>
+          <li><a href="https://myaccount.laxmicoin.com/" style="border:1px solid;margin:11px;padding:11px;" class="js-target-scroll">SignIn</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navigation end --> 
+</header>
+<!-- /Header --> 
+
+<section id="intro" class="section-padding text-center">
+	<div class="container">
+    	<div class="intro_text">
+	    	<h1>Decentralized platform</br> for consumers around the world</h1>
+            <a href="https://laxmicoin.com/wp-laxmicoin.pdf" class="btn" target="_blank">Download WhitePaper</a>
+
+            
+        </div>
+    </div>
+</section>
+
+<section id="about_us" class="section-padding">
+	<div class="container">
+        <div class="row">
+            <div class="col-md-7 col-md-push-5">
+                <h2>About Us</h2>
+                <p>Laxmicoin® is a blockchain platform powered by billions of devices. Started in 2012, we are on a mission to make  the most powerful infrastructure for decentralized applications.</p>
+
+            </div>
+            <div class="col-md-5 col-md-pull-7 text-center">
+            	<img src="images/banner31514026913uOz80.png" alt="image">
+            </div>
+    	</div>
+    </div>
+</section>
+
+<section id="road_map" class="color_bg">
+	<div class="shape_img"><img src="images/top_shape.png" alt="image"></div>
+	<div class="container">
+    	<div class="section-padding">
+    		<div class="text-center white_text">
+            	<h2>Road Map</h2>
+                <p>Witness our Journey and future plans to revolutionize the world</p>
+            </div>
+        </div>
+        
+        <div >
+        	<ul id='timeline'>
+              <li class='entry'>
+                <input checked='checked' class='radio' id='trigger1' name='trigger' type='radio'>
+                    <label for='trigger1'>
+                      <span>
+                        We started our Journey.
+                      </span>
+                    </label>
+                    <span class='date'>2012</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger2' name='trigger' type='radio'>
+                    <label for='trigger2'>
+                      <span>
+                        Relaunch
+                      </span>
+                    </label>
+                    <span class='date'>Q3 2017</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger3' name='trigger' type='radio'>
+                    <label for='trigger3'>
+                      <span>
+                        Laxmicoin Launch
+                      </span>
+                    </label>
+                    <span class='date'>Q2 2018</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger4' name='trigger' type='radio'>
+                    <label for='trigger4'>
+                      <span>
+                       Mobile mining & P2P transactions
+                      </span>
+                    </label>
+                    <span class='date'>Q3 2018</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger5' name='trigger' type='radio'>
+                    <label for='trigger5'>
+                      <span>
+                        Rover Network Alpha Launch
+                      </span>
+                    </label>
+                    <span class='date'>Q1 2019</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger6' name='trigger' type='radio'>            
+                    <label for='trigger6'>
+                      <span>
+                        Rover Storage
+                      </span>
+                    </label>
+                    <span class='date'>Q3 2019</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger7' name='trigger' type='radio'>            
+                    <label for='trigger7'>
+                      <span>
+                        Rover based Apps(Voting Platform)
+                      </span>
+                    </label>
+                    <span class='date'>Q1 2020</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+              <li class='entry'>
+                <input class='radio' id='trigger8' name='trigger' type='radio'>            
+                    <label for='trigger8'>
+                      <span>
+                        More Rover based Apps
+                      </span>
+                    </label>
+                    <span class='date'>2022</span>
+                    <span class='circle'></span>
+                <div class='content'></div>
+              </li>
+            </ul>
+            
+        </div>
+    </div>
+    
+    <div class="shape_img"><img src="images/bottom_shape.png" alt="image"></div>
+</section>
+
+<section id="socialserv" class="section-padding">
+    <div class="container">
+    	<div class="section-padding">
+    		<div class="section_heading">
+            	<h2>Rover Network</h2>
+                <p>Most advanced and secure network for digital world</p>
+            </div>
+            
+            <div class="row">
+            	<div class="col-sm-4">
+                	<div class="features">
+                    	<div class="features_icon"><img src="images/scability.png" alt="image"></div>
+    	            	<h5>SCALABILITY</h5>
+	                    <p>Designed for handling thousands of transactions per second</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                	<div class="features">
+                    	<div class="features_icon"><img src="images/securitylock.png" alt="image"></div>
+    	            	<h5>BEST SECURITY</h5>
+	                    <p>Powered by billions of devices & secured with distributed POS system</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                	<div class="features">
+                    	<div class="features_icon"><img src="images/nominalfee.png" alt="image"></div>
+    	            	<h5>NOMINAL FEE</h5>
+	                    <p>Nominal fee per transactions to maintain network and prevention from DDoS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="section-padding">
+            <div class="section_heading">
+                <h2>Rover Apps</h2>
+                <p>Breaking the barriers</p>
+            </div>
+            
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="features">
+                        <div class="features_icon"><img src="images/banking-solution.png" alt="image"></div>
+                        <h5>Banking Solution</h5>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="features">
+                        <div class="features_icon"><img src="images/good-governance.png" alt="image"></div>
+                        <h5>Good Governance</h5>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="features">
+                        <div class="features_icon"><img src="images/supply-chain.png" alt="image"></div>
+                        <h5>Supply Chain</h5>
+                    </div>
+                </div>
+            </div></br>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="features">
+                        <div class="features_icon"><img src="images/voting-icon.png" alt="image"></div>
+                        <h5>Voting System</h5>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="features">
+                        <div class="features_icon"><img src="images/cyber-security.png" alt="image"></div>
+                        <h5>Cyber Security</h5>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="features">
+                        <div class="features_icon"><img src="images/ride-share.png" alt="image"></div>
+                        <h5>Ride Share</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+</section>
+
+<section id="open_job" class="color_bg white_text">
+	<div class="shape_img"><img src="images/top_shape.png" alt="image"></div>
+	<div class="container">
+    	<div class="section-padding">
+    		<div class="section_heading white_text">
+            	<h2>Join Laxmicoin®</h2>
+            </div>
+            
+            <div class="row">
+            	<div class="col-md-3">
+                	<div class="text-center">
+	                	<img src="images/placeholder_team.png" alt="image">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                	<div class="job_email">
+                    	<h5>hr@laxmicoin.com</h5>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                	<div class="job_info">
+	                	<p>We at Laxmicoin believe that the world is full of highly skilled individuals who are run by passion and possess the requisite integrity for making any business house a more competent and professional place. If you think you can help us in our endeavours somehow, we invite you to join Laxmicoin. You can e-mail us.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="shape_img"><img src="images/bottom_shape.png" alt="image"></div>
+</section>
+
+<section id="newsletter" class="section-padding">
+	<div class="container">
+    	<div class="section_heading">
+        	<h2>Signup Now !</h2>
+            <p>Signup to get rewarded in biggest Airdrop by the laxmicoin.</p>
+        </div>
+        <div class="newsletter_wrap">
+            
+            <div class="signupbtn">
+                <a href="https://myaccount.laxmicoin.com/register" target="_blank">
+                <input type="submit" value="Sign Up Now" name="Sign Up Now" class="btn signup"></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="seen_on" class="color_bg white_text">
+	<div class="shape_img"><img src="images/top_shape.png" alt="image"></div>
+    <div class="section-padding"></div>
+	<div class="container">
+    	<div class="section_heading white_text">
+            <h2>As seen On</h2>
+        </div>
+        <ul class="as_seen_logo">
+            <li><a href="https://cryptonews.com/news/an-interview-with-the-founder-of-india-s-first-cryptocurrenc-1159.htm" target="_blank"><img src="news/cryptonews.jpg" alt="image"></a></li>
+            <li><a href="https://yourstory.com/2017/11/what-is-laxmicoin-indian-cryptocurrency/" target="_blank"><img src="news/ys.png" alt="image"></a></li>
+
+            <li><a href="http://www.firstpost.com/business/laxmicoin-indias-own-bitcoin-seeks-regulatory-clarity-for-launch-1326881.html" target="_blank"><img src="news/fp.png" alt="image"></a></li>
+
+            <li><a href="https://www.coindesk.com/laxmicoin-struggles-to-launch/" target="_blank"><img src="news/header-logo-new.png" alt="image"></a></li>
+
+            <li><a href="http://www.businesstoday.in/opinion/columns/bitcoins-and-laxmicoins-legal-or-not/story/202387.html" target="_blank"><img src="news/logo-btoday.png" alt="image"></a></li>
+
+            <li><a href="http://profit.ndtv.com/news/forex/article-bitcoin-impact-laxmicoin-seeks-regulatory-clarity-for-launch-376940" target="_blank"><img src="news/profit_logo_without_nse.png" alt="image"></a></li>
+
+            <li><a href="http://archive.indianexpress.com/news/laxmicoin-seeks-regulatory-clarity/1216703/" target="_blank"><img src="news/the-indian-express.jpg" alt="image"></a></li>
+
+            <li><a href="http://www.thehindu.com/business/Economy/bitcoin-impact-laxmicoin-seeks-regulatory-clarity-for-launch/article5549324.ece" target="_blank"><img src="news/thfooterlogo.png" alt="image"></a></li>
+
+            <li><a href="https://economictimes.indiatimes.com/tech/internet/bitcoin-impact-laxmicoin-seeks-regulatory-clarity-for-launch/articleshow/28516812.cms" target="_blank"><img src="news/eco.jpg" alt="image"></a></li>
+            <li><a href="https://bitcointalk.org/index.php?topic=287485.0" target="_blank"><img src="news/bitcointalk_logo2.png" alt="image"></a></li>
+
+
+
+        </ul>
+    </div>
+</section>
+
+<footer id="footer">
+	<div class="container">
+    	<div class="footer_text">
+        	<p>The information contained on Laxmicoin’s website (“Site”) is for general information purposes only and should not be taken as financial, advisory or legal advice. No advisor-client relationship attaches to any information contained on the Site. If you have a specific concern, please contact a financial advisor, accountant or attorney to assist with your query. Reference to third-party resources and materials contained within the Site are provided for informational purposes only and should not be viewed as an endorsement of their contents, warranties, accuracy, and representations.</p>
+
+<p>By using this Site, you agree to indemnify and hold Laxmicoin, the organization responsible for creating this Site, harmless from any claim or demand, including reasonable attorney’s fees, initiated by you due to or arising out of relying on this Site in any form or fashion.</p>
+
+<p>While care and due diligence were taken to research and draft information contained this Site, Laxmicoin does not guarantee the accurateness or completeness of this Site nor the provision that statutes, legislation, and regulations referred to in this Site are up-to-date. For updates or changes to cryptocurrency regulations, visit the court registry in your jurisdiction or speak to a qualified professional in the appropriate field.
+</p>
+        </div>
+        <div class="footer_bottom">
+        	<div class="row">
+                <div class="col-md-6">
+                    <p> Copyright 2018. All Rights Reserved by Laxmicoin®</p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="follow_us">
+                        <li class="medium_icon"><a href="https://github.com/laxmicoinofficial" target="_blank"><i class="fa fa-github"></i></a></li>
+                        <li class="facebook_icon"><a href="https://www.facebook.com/laxmicoin/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li class="twitter_icon"><a href="https://twitter.com/Laxmicoin/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                        <li class="medium_icon"><a href="https://medium.com/@laxmicoin" target="_blank">M</a></li>
+                        <li class="youtube_icon"><a href="https://www.youtube.com/c/Laxmicoin" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                        <li class="twitter_icon"><a href="https://t.me/Laxmicoinofficial" target="_blank"><i class="fa fa-telegram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Scripts --> 
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript">
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $(".navbar-collapse").removeClass("in");
+       $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top-40
+        }, 800);
+    });
+</script>
+</body>
+</html>
